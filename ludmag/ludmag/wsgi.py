6df0fe68dd14c70f6bf4244d81c0684a8d8681d0
arheliC <arhelicortes2303@gmail.com>
2020-08-15 00:51:49 -0500

    configuracion requirments, se agrego al basedir static en settings y archivo wsgi

--- ludmag/ludmag/wsgi.py
+++ ludmag/ludmag/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludmag.settings')
 
 application = get_wsgi_application()
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludmag.settings')
+application = Cling(get_wsgi_application())
+

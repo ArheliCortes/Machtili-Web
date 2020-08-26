89c2fef8fe24ec0425f69a6e7d12010fdaa08cc2
arheliC <arhelicortes2303@gmail.com>
2020-08-26 15:36:24 -0500

    diseño móvil

--- ludmag/ludmag/wsgi.py
+++ ludmag/ludmag/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -22,3 +22,4 @@ from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludmag.settings')
 application = Cling(get_wsgi_application())
 
+"""

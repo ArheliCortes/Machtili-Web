c9fdc3b0137e88d15f1d1263d606f599a50b892f
arheliC <arhelicortes2303@gmail.com>
2020-08-26 16:27:49 -0500

    recuperacion diseño movil

--- ludmag/ludmag/settings.py
+++ ludmag/ludmag/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER="humad.ludimagistri@gmail.com"
-EMAIL_HOST_PASSWORD ="Lucy&Adry2020"
+EMAIL_HOST_PASSWORD ="Machtili2020"
 EMAIL_PORT =587

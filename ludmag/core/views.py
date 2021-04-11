aa14ae675e85c1af940289cf37b52f40fcf5840f
ArheliCortes <arhelicortes2303@gmail.com>
2021-04-10 19:11:45 -0500

    Diseño Sección 'Nosotros' cambiadot

--- ludmag/core/views.py
+++ ludmag/core/views.py
@@ -9,19 +9,18 @@ from django.core.mail import BadHeaderError
 from .form import *
 from .models import ProfesorResume,Paragraph
 from django.urls import resolve
-# Create your views here.
+# Global variable
+context = {'form': ClientePotencialForm(),
+           'form_doubt':FormDoubts(),
+           'profesor':ProfesorResume.objects.all()}
+template_home = "core/home.html"
 
 def home (request):
     if request.method == "POST" :
         response = sendEmailInfo(request)
-    else:
-        context = {} 
-        context['form'] = ClientePotencialForm() 
-        context['form_doubt'] = FormDoubts()
-        context['profesor'] = ProfesorResume.objects.all()
-        template_name = "core/home.html"
-        response = render(request,template_name,context)    
-    return response 
+    else:  
+        response = render(request,template_home,context)   
+    return response
 
 def resume(request,profesor_id=1):
     try:
@@ -39,21 +38,10 @@ def doubt (request):
     if request.method == "POST" :
         response = sendEmailDoubt(request)
     else:
-        context = {} 
-        context['form'] = ClientePotencialForm() 
-        context['form_doubt'] = FormDoubts() 
-        template_name = "core/home.html"
-        response = render(request,template_name,context) 
+        response = render(request,template_home,context) 
     return response 
-
 def sendEmailInfo(request):
     form = ClientePotencialForm(request.POST)
-    form_info = ClientePotencialForm() 
-    form_name_info='form'
-    form_doubt = FormDoubts()
-    form_name_doubt='form_doubt'
-    context = {form_name_info: form_info,form_name_doubt:form_doubt}
-
     if form.is_valid():
         name = form.cleaned_data['name']
         lastname = form.cleaned_data['lastname']
@@ -69,27 +57,17 @@ def sendEmailInfo(request):
         to = "arhelicortes2303@gmail.com"
         text_content = "Contactate con :"+name+" "+lastname+"tel: "+str(phone)+" Le interesa el  curso:"+curso+" del grado:"+grado+"Plan:"+plan
         html_content = "<p>Contactate con : <strong>"+name+" "+lastname+"</strong> <br> tel: <strong>"+str(phone)+"</strong><br> correo: <strong>"+email+"</strong><br> Le interesa el  curso:<strong>"+curso+"</strong><br>Grado escolar/Días:<strong>"+grado+"</strong><br>Plan:<strong>"+plan+"</strong></p>"
-
         msg_response = "Su información fue enviada!. Un asesor académico se comunicará con usted en breve."
-        response = _sendEmail(request,subject,text_content,html_content,from_email,to,msg_response,form_name_info,form_info,form_name_doubt,form_doubt)
+        response = _sendEmail(request,subject,text_content,html_content,from_email,to,msg_response)
     else :
-        response = render(request, 'core/home.html', context)
+        response = render(request, template_home, context)
     return response
-
-
 def sendEmailDoubt(request):
     form = FormDoubts(request.POST)
-    form_info = ClientePotencialForm() 
-    form_name_info='form'
-    form_doubt = FormDoubts()
-    form_name_doubt='form_doubt'
-
     if form.is_valid():
         name = form.cleaned_data['name']
         phone = form.cleaned_data['phone']
         email = form.cleaned_data['email']
-        context = {form_name_info: form_info,form_name_doubt:form_doubt}
-
         subject = "Un cliente tiene una duda"
         msg = form.cleaned_data['message']
         from_email = "Machtili<"+settings.EMAIL_HOST_USER+">"
@@ -98,16 +76,14 @@ def sendEmailDoubt(request):
         text_content = msg+"Contactate con :"+name+"email: "+email+"tel: "+str(phone)
         html_content = "<p>Contactate con : <strong>"+name+"</strong><br>email:<strong>"+email+"</strong><br>tel:<strong>"+str(phone)+"</strong><br><br>"+msg+"</p>"
         msg_response = "Su mensaje fue enviado. Un asesor académico tratara de aclarar todas sus dudas lo antes posible."
-        response = _sendEmail(request,subject,text_content,html_content,from_email,to,msg_response,form_name_info,form_info,form_name_doubt,form_doubt)
+        response = _sendEmail(request,subject,text_content,html_content,from_email,to,msg_response)
     else :
-        response = render(request, 'core/home.html', context)
+        response = render(request, template_home, context)
     return response
-
-def _sendEmail(request,subject,text_content,html_content,from_email,to,msg_response,form_name_info,form_info,form_name_doubt,form_doubt):
+def _sendEmail(request,subject,text_content,html_content,from_email,to,msg_response):
 
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(html_content, "text/html")
-    context={form_name_info: form_info,form_name_doubt:form_doubt}
 
     if subject and msg and from_email:
         try:
@@ -117,9 +93,9 @@ def _sendEmail(request,subject,text_content,html_content,from_email,to,msg_respo
             return HttpResponse('Se encontró un encabezado no válido.')
     else:
         messages.warning(request, 'Asegúrese de que todos los campos estén ingresados ​​y sean válidos.')
-        return render(request, 'core/home.html',context)
+        return render(request, template_home,context)
     
-    return render(request, 'core/home.html',context)
+    return render(request,template_home,context)
 
 
 def stop (request):
